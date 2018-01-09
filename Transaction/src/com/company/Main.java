278c720632c839b9028c7b28660a158f44534efb
Soksan <sereysoksan16@kit.edu.kh>
2018-01-09 09:46:46 +0700

    add transaction program

---
+++ Transaction/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.sql.Connection;
+
+public class Main
+{
+
+    public static Connection getCon()
+    {
+
+    }
+    public static void main(String[] args)
+    {
+
+    }
+}
